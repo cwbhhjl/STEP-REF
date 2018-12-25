f21fffb12165891324f3e2f13f8e10db1005dd7c
cwbhhjl <cwbhhjl@gmail.com>
2018-12-25 16:57:50 +0800

    remove demo code

--- src/extension.ts
+++ src/extension.ts
@@ -175,18 +175,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('step active.');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	
 	context.subscriptions.push(vscode.languages.registerHoverProvider(STEP_MODE, new StepHoverProvider()));
 	context.subscriptions.push(new SchemaInfoController());
 }
